ba2ae7c22fd74e0f5918449098a3a9fd0c868438
dimitripedinielli <93774803+dimitripedinielli@users.noreply.github.com>
2022-03-11 12:49:35 +0100

    Add files via upload

---
+++ Heros.java
@@ -0,0 +1,7 @@
+public class Heros extends main {
+    public String Hunter;
+    public String Warrior;
+    public String Mage;
+    public String Healer;
+
+}
---
+++ Hunter.java
@@ -0,0 +1,9 @@
+public class Hunter extends Heros {
+    public int nbPotion;
+    public int PV;
+    public int food;
+    public int arme;
+    public int armure;
+
+
+}
